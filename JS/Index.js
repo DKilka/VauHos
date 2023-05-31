bf25c9526f607edc9adc1479fec5477b5381c8d8
NovaJulius <dushenko.m21@cf.dnu.dp.ua>
2023-05-31 12:29:18 +0300

    f

--- JS/Index.js
+++ JS/Index.js
@@ -1,4 +1,4 @@
-const images = [
+const desktopImages = [
   "../IMG/slider_furnit_01.jpg",
   "../IMG/slider_furnit_02.jpg",
   "../IMG/slider_furnit_03.jpg",
@@ -10,17 +10,46 @@ const images = [
   "../IMG/зображення_viber_2023-05-30_13-49-17-496.jpg"
 ];
 
+const mobileImages = [
+  "../IMG/sTel_furnit_01.jpg",
+  "../IMG/sTel_furnit_02.jpg",
+  "../IMG/sTel_furnit_03.jpg",  
+  "../IMG/sTel_furnit_04.jpg",
+  "../IMG/sTel_furnit_05.jpg",
+  "../IMG/sTel_furnit_06.jpg",  
+  "../IMG/sTel_shower_01.jpg",
+  "../IMG/sTel_shower_02.jpg",
+  "../IMG/sTel_shower_03.jpg",
+  "../IMG/sTel_shower_04.jpg",
+  "../IMG/sTel_shower_05.jpg",
+  "../IMG/sTel_shower_06.jpg",
+  "../IMG/sTel_glass_01.webp",
+  "../IMG/sTel_glass_02.webp",
+  "../IMG/sTel_glass_03.webp",
+  "../IMG/sTel_glass_04.jpg",
+  "../IMG/sTel_glass_05.jpg",
+  "../IMG/sTel_glass_06.jpg",
+  "../IMG/sTel_glass_07.jpg",
+];
+
 window.addEventListener("DOMContentLoaded", function () {
-  slide.src = images[currentSlideIndex];
+  updateSlide();
 });
 
 let currentSlideIndex = 0;
 let isHovered = false;
+let images = [];
 
 const slideContainer = document.querySelector(".slider");
 const slide = document.querySelector(".slide");
 const prevButton = document.querySelector(".prev-button");
 const nextButton = document.querySelector(".next-button");
+
+function updateSlide() {
+  const isMobile = window.innerWidth < 1200;
+  images = isMobile ? mobileImages : desktopImages;
+  slide.src = images[currentSlideIndex];
+}
 
 function showSlide(index) {
   slide.style.opacity = 0;
@@ -35,11 +64,7 @@ function nextSlide() {
   if (currentSlideIndex >= images.length) {
     currentSlideIndex = 0;
   }
-  slide.style.opacity = 0;
-  setTimeout(function () {
-    slide.src = images[currentSlideIndex];
-    slide.style.opacity = 1;
-  }, 300);
+  showSlide(currentSlideIndex);
 }
 
 function prevSlide() {
@@ -47,11 +72,7 @@ function prevSlide() {
   if (currentSlideIndex < 0) {
     currentSlideIndex = images.length - 1;
   }
-  slide.style.opacity = 0;
-  setTimeout(function () {
-    slide.src = images[currentSlideIndex];
-    slide.style.opacity = 1;
-  }, 300);
+  showSlide(currentSlideIndex);
 }
 
 function handleMouseEnter() {
@@ -71,13 +92,17 @@ nextButton.addEventListener("click", function () {
     nextSlide();
   }
 });
+
 prevButton.addEventListener("click", function () {
   if (!isHovered) {
     prevSlide();
   }
 });
+
 slideContainer.addEventListener("mouseenter", handleMouseEnter);
 slideContainer.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", updateSlide);
 
 setInterval(function () {
   if (!isHovered) {
